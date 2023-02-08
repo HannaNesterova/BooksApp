3b52e443a1dca6e37cf8cee9816a57e016405b5f
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-02-08 23:13:44 +0100

    initAction function

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,22 @@
 {
     'use strict';
-    let selected = dataSource.books;
-    console.log(selected);
-  
+    const selected = dataSource.books;
+
     const select = {
       templateOf: {
          book: '#template-book',
       },
       containerOf: {
       container: '.books-list',
+
     },
      image:{
-      imageWrapper: '.book__image',
+      imageWrapper: '.books-list .book__image',
      },
     };
-    
     const templates = {
       books: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),   
     }; 
-     console.log(templates);
   
   
     function renderInList() {
@@ -26,23 +24,51 @@
               
       /* generate HTML based on template (генерувати HTML на основі шаблона)*/ 
       const generatedHTML = templates.books(book);
-      console.log(generatedHTML);
   
       /* create element using utils.createElementFromHTML(створити  елемент за допомогою utils.createElementFromHTML)*/
       const generatedDOMElement = utils.createDOMFromHTML(generatedHTML);
   
       /* find menu container (знайти контейнер )*/
        const bookContainer = document.querySelector(select.containerOf.container);
-       console.log(bookContainer);
   
       /* add element to menu (додати елемент до меню)*/
        bookContainer.appendChild(generatedDOMElement);
       }
     };
     renderInList();
-    
+
+
+
+    //add empty arr 
+    const favoriteBooks = [];
+
+    //create a function initActions.
+    function initActions(){
+
+    //find Ref for. book__image у .booksList   
+    const booksRef = document.querySelectorAll(select.image.imageWrapper);
+    console.log('ref', booksRef);
+  
+    //FOR for every books element
+    for(ref of booksRef){
+        //add db listener
+        ref.addEventListener('dbсlick', function (e){
+            e.preventDefoult();
+        })
+    }
+    //if true use preventDefoult
+    if(ref){
+    // add class favorite for shoosen element
+    ref.classList.add('favorite');
+    }
+    //dowload ID from data-id
+    const booksId = booksRef.getAttribute('getAttribute');
+    console.log(booksId)
+
+    // add push ID to the favoriteBooks
+    favoriteBooks.push(booksId);
+    }
   }
-  
-  
+  initActions();
   
     
